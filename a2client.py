ac405c1af811d375bd27ded73523d54415a0caaa
sofia.sugar <sofiabesenski4@gmail.com>
2019-10-28 15:04:25 -0700

    implemented client

--- a2client.py
+++ a2client.py
@@ -14,17 +14,25 @@ CLIENT_IP= sys.argv[1]
 CLIENT_PORT = int(sys.argv[2])
 
 client_socket.bind((CLIENT_IP,CLIENT_PORT))
-sequence_number = 122
 client_socket.settimeout(1)
+SERVER_IP = sys.argv[3]
+SERVER_PORT = sys.argv[4]
 
 
-SERVER_IP = "127.0.0.1"
-SERVER_PORT = 12000
-sending_time = time.time()
-message = "ping {} {}".format(sequence_number, sending_time) 
-client_socket.sendto(message, (SERVER_IP,SERVER_PORT))
+print "Communicating with a server at adr {} and port {}".format(str(SERVER_IP),str(SERVER_PORT))	
+i = 1
+while (i<11):
+	try:
+		sending_time=time.time()
+		message = "ping {} {}".format(i, sending_time) 
+		client_socket.sendto(message, (SERVER_IP,SERVER_PORT))
 
 
-response, message = client_socket.recvfrom(1024)
-response_time = time.time() - sending_time
-print response, "in millisecond:", str(response_time*1000)
+		response, message = client_socket.recvfrom(1024)
+		response_time = time.time() - sending_time
+		print response, " RTT in milliseconds:", str(response_time*1000)
+		
+		
+		i+=1;
+	except:
+		print "Request timed out"
